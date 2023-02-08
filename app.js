73054a316ec337667fb5f8b4d1a018438cce0cb1
abostaf <23037172@student.ltuc.com>
2023-02-08 11:36:34 +0000

    the last one ea rab

--- app.js
+++ app.js
@@ -27,28 +27,59 @@ if (skip === false) {
 
 
 }
+let arr =[];
 let question1 = prompt("Do you love JavaScript?");
 let question2 = prompt("Does JavaScript easy language?");
 let question3 = prompt("Does JavaScript have function?");
 
-if (question1 === "yes" || question1 === "no") {
-  console.log(question1);
-}
-else {
-  question1 = "invalid";
-}
-if (question2 === "yes" || question2 === "no") {
-  console.log(question2);
-}
-else {
-  question2 = "invalid";
-}
-if (question3 === "yes" || question3 === "no") {
-  console.log(question3);
-}
-else {
-  question3 = "invalid";
+function compare(m)
+{
+  if (m === "yes" || m === "no") 
+  {
+    arr.push(m);
+  }
+  else
+  {
+    arr.push("invalid");
+  }
+
 }
 
-let answer = [question1, question2, question3];
-console.log(answer);
+function pushh(){
+  for(let i = 0; i<arr.length; i++)
+  {
+    console.log(arr[i]);
+  }
+}
+compare(question1);
+compare(question2);
+compare(question3);
+console.log(arr);
+pushh();
+
+
+
+
+// if (question1 === "yes" || question1 === "no") {
+//   console.log(question1);
+// }
+// else {
+//   question1 = "invalid";
+// }
+// if (question2 === "yes" || question2 === "no") {
+//   console.log(question2);
+// }
+// else {
+//   question2 = "invalid";
+// }
+// if (question3 === "yes" || question3 === "no") {
+//   console.log(question3);
+// }
+// else {
+//   question3 = "invalid";
+// }
+
+// let answer = [question1, question2, question3];
+// console.log(answer);
+
+
